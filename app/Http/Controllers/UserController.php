e6efedb0d3f7ff264b39c8b27181f21402e0a352
Aljirana Mawaddah <aljiranamawaddah1612-dev.com>
2026-07-04 02:07:20 +0700

    Pertemuan 12:User Create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -13,6 +14,7 @@ class UserController extends Controller
     {
         return view('user.index', [
             'title' => 'User',
+            'users' => User::latest()->get()
         ]);
     }
 
@@ -21,7 +23,9 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+         return view('user.create', [
+            'title' => 'Tambah User'
+        ]);
     }
 
     /**
@@ -29,7 +33,38 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8',
+            'passwordconfirm' => 'required|same:password',
+            'avatar' => 'nullable|image|mimes:jpeg,png,jpg|max:1048', // Optional: if uploading a file
+            'role' => 'required|in:Superadmin,Admin',
+        ], [
+            'name.required' => 'Nama tidak boleh kosong.',
+            'email.required' => 'Email tidak boleh kosong.',
+            'email.unique' => 'Email sudah terdaftar.',
+            'password.required' => 'Password tidak boleh kosong.',
+            'passwordconfirm.required' => 'Konfirmasi password tidak boleh kosong.',
+            'passwordconfirm.same' => 'Konfirmasi password tidak cocok.',
+            'role.required' => 'Role harus dipilih.',
+            'role.in' => 'Role harus berupa Superadmin atau Admin.',
+        ]);
+
+        try{
+
+if($request->file('avatar')){
+            $validated['avatar'] = $request->file('avatar')->store('avatar', 'public');
+        }
+
+            DB::beginTransaction();    
+           User::create($validated);
+            DB::commit();
+            return to_route('user.index')->withSuccess('Data berhasil di tambahkan');
+        }catch(\Exception $e){
+            DB::rollback();
+            return to_route('user.create')->withError('Data gagal ditambahkan');    
+        }
     }
 
     /**
